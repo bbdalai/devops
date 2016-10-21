c6664b2195d49e1b1f1cc8c3001f93747034a236
inbda <bbdalai@gmail.com>
2016-10-21 14:06:12 +0530

    demo project

---
+++ demo/code/source/demo.java
@@ -0,0 +1,41 @@
+
+
+// -----( IS Java Code Template v1.2
+// -----( CREATED: 2016-10-21 14:05:02 IST
+// -----( ON-HOST: centos.inbda.01
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class demo
+
+{
+	// ---( internal utility methods )---
+
+	final static demo _instance = new demo();
+
+	static demo _newInstance() { return new demo(); }
+
+	static demo _cast(Object o) { return (demo)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void hello (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(hello)>> ---
+		// @sigtype java 3.5
+		IDataUtil.put(pipeline.getCursor(), "result", "Hello World!!!");
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
---
+++ demo/ns/demo/hello/java.frag
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<Values version="2.0">
+  <value name="name">hello</value>
+  <value name="encodeutf8">true</value>
+  <value name="body">SURhdGFVdGlsLnB1dChwaXBlbGluZS5nZXRDdXJzb3IoKSwgInJlc3VsdCIsICJIZWxsbyBXb3Js
+ZCEhISIpOw==</value>
+</Values>

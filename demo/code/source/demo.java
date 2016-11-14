0767ef5bf0ff806d68eff83b20bbfe876d1c35ef
user1 <user1@centos.inbda.01>
2016-11-14 14:29:48 +0530

    C1

--- demo/code/source/demo.java
+++ demo/code/source/demo.java
@@ -1,7 +1,7 @@
 
 
 // -----( IS Java Code Template v1.2
-// -----( CREATED: 2016-10-26 13:06:17 IST
+// -----( CREATED: 2016-11-14 14:28:26 IST
 // -----( ON-HOST: centos.inbda.01
 
 import com.wm.data.*;
